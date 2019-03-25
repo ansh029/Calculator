51c03347dab58a40dfc95a06249258496d5adb60
Anshul <anshul@knstek.com>
2019-03-25 12:23:38 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, View} from 'react-native';
+import Calc from './Components/Calc'
+
+
+export default class App extends Component{
+  render() {
+    return (
+      
+      <Calc/>
+      
+    );
+  }
+}
+
